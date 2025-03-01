c055be072c2facd2c1086a89f6860b5348f1b5e8
Ваше ім'я < email>
2025-03-01 21:54:41 +0200

    minimum style

--- src/components/Feedback/FeedBack.jsx
+++ src/components/Feedback/FeedBack.jsx
@@ -2,19 +2,24 @@ import React from "react";
 
 export default function Feedback({ feedback }) {
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-  const positiveFeedback =
-  totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
+  
 if(totalFeedback === null){
   return
 }
   return (
-    <div className="feedback">
-      <p>Good: {feedback.good}</p>
-      <p>Neutral: {feedback.neutral}</p>
-      <p>Bad: {feedback.bad}</p>
-      <p>Total: {totalFeedback}</p>
-      <p>Positive: {positiveFeedback}%</p>
-    </div>
+    <ul>
+    <li>Good: {feedback.good}</li>
+    <li>Neutral: {feedback.neutral}</li>
+    <li>Bad: {feedback.bad}</li>
+    <li>Total: {feedback.good + feedback.neutral + feedback.bad}</li>
+    <li>
+      Positive:{" "}
+      {feedback.good + feedback.neutral + feedback.bad > 0
+        ? Math.round((feedback.good / (feedback.good + feedback.neutral + feedback.bad)) * 100)
+        : 0}
+      %
+    </li>
+  </ul>
   );
 }
 
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,23 +1,12 @@
 import React from "react";
+import style from './options.module.css'
 
 export default function Options({ feedback, onLeaveFeedback, onReset }) {
     const total = feedback.good + feedback.neutral + feedback.bad;
 
   return (
-    <div className="options">
-      <ul>
-        <li>Good: {feedback.good}</li>
-        <li>Neutral: {feedback.neutral}</li>
-        <li>Bad: {feedback.bad}</li>
-        <li>Total: {feedback.good + feedback.neutral + feedback.bad}</li>
-        <li>
-          Positive:{" "}
-          {feedback.good + feedback.neutral + feedback.bad > 0
-            ? Math.round((feedback.good / (feedback.good + feedback.neutral + feedback.bad)) * 100)
-            : 0}
-          %
-        </li>
-      </ul>
+    <div className={style.btnsection}>
+    
       <button onClick={() => onLeaveFeedback("good")}>Good</button>
       <button onClick={() => onLeaveFeedback("neutral")}>Neutral</button>
       <button onClick={() => onLeaveFeedback("bad")}>Bad</button>
